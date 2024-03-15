40c3f90c49a7d6caa6eb75ff55c006c0ba1e23c4
mrwhittleman <161540900+mrwhittleman@users.noreply.github.com>
2024-03-15 01:04:51 -0700

    Update index.tsx

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,4 +2,4 @@ export const WALLET_CONNECT_PROJECT_ID = process.env.WALLET_CONNECT_PROJECT_ID ?
 export const NETWORK = (process.env.NETWORK ?? 'test') as 'main' | 'test'
 export const NODE_URL = process.env.NODE_URL ?? `https://node-${NETWORK}net.vechain.energy`
 export const SPONSORSHIP_URL = process.env.SPONSORSHIP_URL ?? 'https://sponsor-testnet.vechain.energy/by/90'
-export const SLAYER_WALLET =  process.env.SLAYER_WALLET ?? '0x0000000000000000000000000000456e65726779'
+export const SLAYER_WALLET =  process.env.SLAYER_WALLET ?? '0x3665eD160eDD2bC236fBDA83274eacA08769B0b9'
