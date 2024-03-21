e33c43a6f12a59ca1cbde30a99d0ba1e2b114ac0
Mario Micklisch <mario@favo.org>
2024-03-21 08:37:19 +0100

    Add basic nft listing without using an indexer

--- src/Pages/MySlayer.tsx
+++ src/Pages/MySlayer.tsx
@@ -1,5 +1,18 @@
-export default function MySlayerPage () {
+import { useNftList } from '~/src/hooks/useNftList';
+import { useWallet } from '@vechain/dapp-kit-react';
+
+export default function MySlayerPage() {
+    const { account } = useWallet();
+    const { balance, isLoading, tokens } = useNftList(account);
+
     return (
-        <>My Slayer</>
+        <>
+            <div>My Slayer</div>
+            <div>Your Tokens: {balance}</div>
+            {isLoading
+                ? <div>Loading...</div>
+                : tokens.map(token => (<div key={token.tokenId}>{token.tokenId}: {token.tokenURI}</div>))
+            }
+        </>
     )
 }
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -3,4 +3,4 @@ export const NETWORK = (process.env.NETWORK ?? 'test') as 'main' | 'test'
 export const NODE_URL = process.env.NODE_URL ?? `https://node-${NETWORK}net.vechain.energy`
 export const SPONSORSHIP_URL = process.env.SPONSORSHIP_URL ?? 'https://sponsor-testnet.vechain.energy/by/90'
 export const SLAYER_WALLET =  process.env.SLAYER_WALLET ?? '0x3665eD160eDD2bC236fBDA83274eacA08769B0b9'
-export const SLAYER_CONTRACT =  process.env.SLAYER_CONTRACT ?? ''
+export const SLAYER_CONTRACT =  process.env.SLAYER_CONTRACT ?? '0x84466753b03e2f6d74afe8bf356c09e63dd36d67'
