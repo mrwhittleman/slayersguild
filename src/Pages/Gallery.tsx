2d60d34e79aadbd14fe0575b08b7e2ea3c6277a4
mulekatz <mulekatz@gmail.com>
2024-03-27 09:27:33 +0100

    add basic funcionality

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -1,5 +1,25 @@
-export default function GalleryPage () {
-    return (
-        <>Gallery</>
-    )
-}
+import { useEffect, useState } from "react";
+
+import Details from "@/components/Details";
+import Imageview from "@/components/Imageview";
+import Stats from "@/components/Stats";
+
+const GalleryPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <section className="flex w-full gap-4">
+      <div className="flex flex-col basis-full gap-4">
+        {isOpen && <Details />}
+        <Imageview />
+      </div>
+      {isOpen && <Stats />}
+    </section>
+  );
+};
+
+export default GalleryPage;
