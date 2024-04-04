d94a736d5d653ca1ef96beb33037338fee6d491a
mulekatz <mulekatz@gmail.com>
2024-04-04 03:32:23 +0200

    change new gallery layout

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
-
-import Details from "@/components/Details";
-import Imageview from "@/components/Imageview";
-import Stats from "@/components/Stats";
+import GalleryView from "@/components/GalleryView";
 
 const GalleryPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleClick() {
-    setIsOpen(!isOpen);
-  }
-
   return (
-    <section className="flex w-full gap-4">
-      <div className="flex flex-col basis-full gap-4">
-        {isOpen && <Details />}
-        <Imageview />
-      </div>
-      {isOpen && <Stats />}
+    <section className="flex flex-col w-full gap-4">
+      <GalleryView />
     </section>
   );
 };
