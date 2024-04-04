aad95b39a953781b8953130d2500c5354086b660
mulekatz <mulekatz@gmail.com>
2024-04-04 03:25:57 +0200

    add image modules

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,3 +3,13 @@ interface Window {
     isVeWorld: boolean;
   };
 }
+
+declare module "*.png" {
+  const value: any;
+  export = value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export = value;
+}
