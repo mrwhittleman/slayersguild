49645bfeb46d12c411ac100f7c698f16a650d393
mulekatz <mulekatz@gmail.com>
2024-04-09 17:36:58 +0200

    refactor loading states, add trycatch block,

--- src/hooks/useMetadata.ts
+++ src/hooks/useMetadata.ts
@@ -2,27 +2,33 @@
 import { useState, useEffect } from "react";
 import { NftMetadataType } from "@/types/types";
 
+const METADATA_URL =
+  "https://u3r3iwywduc6bsvgopanuu5ns7x3tetf6uua77ng6zft2cy6pabq.arweave.net/puO0WxYdBeDKpnPA2lOtl--5kmX1KA_9pvZLPQseeAM/";
+
 export const useMetadata = (tokenId: number) => {
   const [metadata, setMetadata] = useState<NftMetadataType>();
-  const [loading, setLoading] = useState(true);
+  const [metadataLoading, setMetadataLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setMetadataLoading(true);
     const fetchMetadata = async () => {
-      const response = await fetch(
-        `https://u3r3iwywduc6bsvgopanuu5ns7x3tetf6uua77ng6zft2cy6pabq.arweave.net/puO0WxYdBeDKpnPA2lOtl--5kmX1KA_9pvZLPQseeAM/${tokenId}.json`
-      );
+      try {
+        const response = await fetch(`${METADATA_URL}${tokenId}.json`);
 
-      if (!response.ok) {
-        throw new Error(`No metadata found for token ID ${tokenId}`);
+        if (!response.ok) {
+          throw new Error(`No metadata found for token ID ${tokenId}`);
+        }
+
+        const metadata = await response.json();
+        setMetadata(metadata);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setMetadataLoading(false);
       }
-
-      const metadata = await response.json();
-      setMetadata(metadata);
-      setLoading(false);
     };
 
     fetchMetadata();
   }, [tokenId]);
-
-  return { metadata, loading };
+  return { metadata, metadataLoading };
 };
