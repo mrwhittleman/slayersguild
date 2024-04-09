eae309e729f194f630111761bfb3516e656ef5a4
mulekatz <mulekatz@gmail.com>
2024-04-09 17:33:18 +0200

    change div to span container to integrate it inot p tags

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,12 +3,9 @@ import { cn } from "@/lib/utils";
 
 const Spinner = ({ className }: { className?: string }) => {
   return (
-    <div
-      className={cn(
-        "animate-spin rounded-full h-8 w-8 border-b-2 border",
-        className
-      )}
-    ></div>
+    <span
+      className={cn("animate-spin rounded-full h-8 w-8 border-b-2", className)}
+    ></span>
   );
 };
 
