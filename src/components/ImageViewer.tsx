ee12d30f89b5f0ad29690b2d3b3d1e1436dc4c59
mulekatz <mulekatz@gmail.com>
2024-04-12 22:02:59 +0200

    Refactor ImageViewer component to use FontAwesomeIcon for magnifying glass icon instead of children prop

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-const ImageViewer = ({
-  children,
-  imageUrl,
-  altName,
-}: {
-  children: React.ReactNode;
+interface ImageViewerProps {
   imageUrl: string;
   altName: string;
-}) => {
+}
+
+const ImageViewer: React.FC<ImageViewerProps> = ({ imageUrl, altName }) => {
   return (
     <Dialog>
-      <DialogTrigger>{children}</DialogTrigger>
+      <DialogTrigger>
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          size="xl"
+          className="absolute bottom-4 right-4 bg-tertiary/45 rounded-lg p-2 hover:bg-tertiary/65 hover:scale-110 transition-all"
+        />
+      </DialogTrigger>
       <DialogContent className="flex p-0 w-full min-w-fit">
         <img className="rounded-lg" src={imageUrl} alt={altName} width={1024} />
       </DialogContent>
