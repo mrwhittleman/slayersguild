de80ad496aa2d606e5145810efbeddcb5c616f31
mulekatz <mulekatz@gmail.com>
2024-04-12 22:08:18 +0200

    Update config/index.tsx to add SLAYER_IMAGE_URL, METADATA_URL, NFTLIST_API_URL, WOV_STAKING_ADDRESS, and MAX_NFT_SUPPLY

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -9,3 +9,15 @@ export const SLAYER_WALLET =
   process.env.SLAYER_WALLET ?? "0x3665eD160eDD2bC236fBDA83274eacA08769B0b9";
 export const SLAYER_CONTRACT =
   process.env.SLAYER_CONTRACT ?? "0x84466753b03e2f6d74afe8bf356c09e63dd36d67";
+export const SLAYER_IMAGE_URL =
+  process.env.SLAYER_IMAGE_URL ??
+  "https://4wz7nuijhm67qgxnqw54egbv52zocgq3qei3gbb7la5vuvowh47q.arweave.net/5bP20Qk7Pfga7YW7whg17rLhGhuBEbMEP1g7WlXWPz8/";
+export const METADATA_URL =
+  process.env.METADATA_URL ??
+  "https://u3r3iwywduc6bsvgopanuu5ns7x3tetf6uua77ng6zft2cy6pabq.arweave.net/puO0WxYdBeDKpnPA2lOtl--5kmX1KA_9pvZLPQseeAM/";
+export const NFTLIST_API_URL =
+  process.env.NFTLIST_API_URL ?? "https://slayersguild-b60.pages.dev/api/nfts";
+export const WOV_STAKING_ADDRESS =
+  process.env.WOV_STAKING_ADDRESS ??
+  "0x4aa8053a7f44aa5ab676f0f2cb9c43ca7ff10395";
+export const MAX_NFT_SUPPLY = Number(process.env.MAX_NFT_SUPPLY) ?? 666;
