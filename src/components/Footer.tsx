e030032dce07be8ec785cfc09fed180efae1e320
mulekatz <mulekatz@gmail.com>
2024-04-12 21:59:45 +0200

    Update Footer component to dynamically display the current year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <footer className="flex w-full p-4 bg-accent justify-center">
-      <p className="text-sm">&copy; 2024 - Vechain Slayers Guild</p>
+    <footer className="flex w-full p-4 bg-accent justify-center z-50">
+      <p className="text-sm">&copy; {year} - Vechain Slayers Guild</p>
     </footer>
   );
 };
