23a8e6af88ecc5506bcdcdf9a66bdcd5b2fde728
mulekatz <mulekatz@gmail.com>
2024-04-12 21:57:45 +0200

    Refactor routing paths in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import App from "@/App";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
-import OfferingPage from "./pages/Offering";
-import AboutPage from "./pages/About";
 import MySlayerPage from "./pages/MySlayer";
 import GalleryPage from "./pages/Gallery";
 import AltarPage from "./pages/Altar";
@@ -16,12 +14,10 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: "/", element: <App /> },
-      { path: "/offering", element: <OfferingPage /> },
-      { path: "/about", element: <AboutPage /> },
       { path: "/my-slayer", element: <MySlayerPage /> },
       { path: "/gallery", element: <GalleryPage /> },
       { path: "/altar", element: <AltarPage /> },
-      { path: "/slayer-details/:id", element: <SlayerDetailsPage /> },
+      { path: "/slayer-details/:tokenId", element: <SlayerDetailsPage /> },
     ],
   },
 ]);
