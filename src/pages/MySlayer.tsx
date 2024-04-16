185f739c185dfdd30d5a7669da49e4dccfbad31e
mulekatz <mulekatz@gmail.com>
2024-04-16 15:54:34 +0200

    add logic to check the staked nfts are originaly owned by the current logged wallet add caching strategy to reduce api spaming add slayercardcomponent to display user owned and staked nfts in jsx add a go to marketplace and buy button if the wallet does not have any slayers

--- src/pages/MySlayer.tsx
+++ src/pages/MySlayer.tsx
@@ -1,29 +1,61 @@
-import { useWalletName } from "@/hooks/useWalletName";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useWallet } from "@vechain/dapp-kit-react";
+import { useWalletName } from "@/hooks/useWalletName";
 import { useNftList } from "@/hooks/useNftList";
-import { truncateMiddle } from "@/lib/utils";
+import { transfersCheck, truncateMiddle } from "@/lib/utils";
+import { WOV_URL } from "@/config";
+import { NftListType, StakedNftType } from "@/types/types";
+import { Grid, GridContent, GridGallery } from "@/components/ui/grid";
 import Spinner from "@/components/Spinner";
 import SlayerCardComponent from "@/components/SlayerCardComponent";
-import { NftListType } from "@/types/types";
-import { Grid, GridContent, GridGallery } from "@/components/ui/grid";
 import CopyClipboard from "@/components/CopyClipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "@/components/ui/button";
 
 const MySlayerPage = () => {
   const wallet = useWallet();
   const { name } = useWalletName(wallet.account);
-  const { balance, isLoading, tokens } = useNftList(wallet.account);
+  const {
+    balance,
+    isLoading: loadingNftList,
+    tokens,
+  } = useNftList(wallet.account);
+  const [stakedTokens, setStakedTokens] = useState<StakedNftType[]>([]);
+  const [loadingTransferCheck, setLoadingTransferCheck] = useState(false);
+  const isLoading = loadingNftList || loadingTransferCheck;
 
-  const handleClick = () => {
-    window.open(
-      "https://worldofv.art/collection/Slayers",
-      "_blank",
-      "noopener,noreferrer"
-    );
-  };
+  // Check if wallet account is connected and check the transfers of the current wallet account for transfers to the staking contract and set the stakedNfts state
+  useEffect(() => {
+    const loadData = async () => {
+      setLoadingTransferCheck(true);
+      const cache = JSON.parse(
+        sessionStorage.getItem("stakedTokensCache") || "{}"
+      );
+      let data = cache.data || [];
+      const lastUpdated = cache.lastUpdated;
 
+      if (!lastUpdated || Date.now() - lastUpdated > 30000) {
+        data = await transfersCheck(wallet.account!);
+        sessionStorage.setItem(
+          "stakedTokensCache",
+          JSON.stringify({
+            data,
+            lastUpdated: Date.now(),
+          })
+        );
+      }
+
+      setStakedTokens(data);
+      setLoadingTransferCheck(false);
+    };
+
+    if (wallet.account) {
+      loadData();
+    }
+  }, [wallet.account]);
+
+  // If wallet account is not connected, display a message to connect the wallet
   if (!wallet.account) {
     return (
       <div className="flex flex-col w-full items-center gap-4">
@@ -33,8 +65,10 @@ const MySlayerPage = () => {
       </div>
     );
   }
+  // If wallet account is connected, display the slayer collection of the wallet account (staked and not staked slayers)
   return (
     <div className="flex flex-col w-full">
+      {/* WELCOME MESSAGE AND WALLET ACCOUNT INFO */}
       <div className="flex w-fit items-end gap-4">
         <h3 className="text-4xl">Skol,</h3>
         <CopyClipboard copyData={wallet.account}>
@@ -48,9 +82,18 @@ const MySlayerPage = () => {
         </CopyClipboard>
       </div>
       <div className="flex h-full w-full flex-col gap-8">
+        {/* USER SLAYER COLLECTION INFO */}
         <p className="flex gap-1">
           You currently own{" "}
-          {isLoading ? <Spinner className="w-3 h-3 self-center" /> : balance}{" "}
+          {isLoading ? (
+            <Spinner className="w-3 h-3 self-center" />
+          ) : tokens.length > 0 ? (
+            balance
+          ) : stakedTokens.length > 0 ? (
+            stakedTokens.length
+          ) : (
+            "0"
+          )}{" "}
           Slayer!
         </p>
         {isLoading ? (
@@ -58,10 +101,12 @@ const MySlayerPage = () => {
             <Spinner />
           </div>
         ) : (
+          /* SLAYER VIEW */
           <Grid className="grid-cols-1 lg:grid-cols-2 divide-x-0 lg:divide-x divide-y lg:divide-y-0">
             <GridContent className="pr-0 lg:pr-8 pb-8 lg:pb-0">
-              {tokens.length > 0 ? (
+              {tokens.length > 0 || stakedTokens.length > 0 ? (
                 <GridGallery>
+                  {/* NOT STAKED SLAYERS */}
                   {tokens.map((token, idx) => {
                     const slayer = {
                       tokenId: token.tokenId,
@@ -77,17 +122,43 @@ const MySlayerPage = () => {
                       />
                     );
                   })}
+                  {/* STAKED SLAYERS */}
+                  {stakedTokens.map(
+                    (token: { tokenId: string }, idx: number) => {
+                      const slayer = {
+                        tokenId: token.tokenId,
+                        owner: wallet.account,
+                      } as NftListType;
+
+                      return (
+                        <SlayerCardComponent
+                          key={`${token.tokenId}-gallery-${idx}`}
+                          slayer={slayer}
+                          type="link"
+                          staked={true}
+                          className="cursor-pointer outline-4 hover:outline"
+                        />
+                      );
+                    }
+                  )}
                 </GridGallery>
               ) : (
+                /* If no slayers found -> info + buy button */
                 <div className="flex flex-col w-full h-full justify-center items-center gap-4">
                   <p className="text-2xl">No Slayer found.</p>
-                  <Button className="flex items-center" onClick={handleClick}>
+                  <Link
+                    to={`${WOV_URL}/collection/Slayers`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center justify-center px-4 py-2 rounded-lg bg-tertiary hover:bg-tertiary/65 hover:scale-110 transition-all"
+                  >
                     <FontAwesomeIcon icon={faCartShopping} size="xl" />
-                  </Button>
+                  </Link>
                 </div>
               )}
             </GridContent>
-            <GridContent className="flex-col gap-4 items-center lg:items-start pl-0 lg:pl-8 pt-8 lg:pt-0">
+            {/* FILLER FOR FUTURE STUFF */}
+            <GridContent className="flex-col gap-4 justify-start lg:items-start pl-0 lg:pl-8 pt-8 lg:pt-0">
               <p className="max-w-md">
                 In the realm of Eldoria, 666 fierce warriors known as Slayers,
                 inspired by Nordic mythology, rose to confront the spreading
